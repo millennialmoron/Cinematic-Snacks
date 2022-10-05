8fa897cfa91ccc5088c2e15277032aae51d7f9bb
millennialmoron <millennialmoron87@gmail.com>
2022-10-05 13:51:36 +0200

    set up footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,46 @@
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image
+          src="/img/Footer.png"
+          layout="fill"
+          objectFit="contain"
+          alt="Movie Theater Popcorn"
+        />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            We make binge-worthy food so you can keep binging TV with ease.
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>Find out if we deliver near you:</h1>
+          <p className={styles.text}>
+            NEW YORK, NY || LOS ANGELES, CA || CHICAGO, IL || AUSTIN, TX
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>Hours of Delivery:</h1>
+          <p className={styles.text}>SUNDAY-THURSDAY:</p>
+          <p className={styles.text}>7am-11pm</p>
+          <p className={styles.text}>FRIDAY & SATURDAY:</p>
+          <p className={styles.text}>7am-1am</p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>Looking for a job?</h1>
+          <p className={styles.text}>
+            We pay great, have awesome benefits, and care about our employees.
+          </p>
+          <p className={styles.text}>
+            EMAIL JOBS@CINEMATICSNACKS.COM FOR MORE INFO!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
